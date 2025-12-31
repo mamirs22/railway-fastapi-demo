61ad02053bf6eecc89dd4568241598d10b52747b
mamirs22 <2311102233@ittelkom-pwt.ac.id>
2025-12-31 14:18:02 +0700

    update commit

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 from fastapi import FastAPI, Query
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import time, socket, os
 
 app = FastAPI()
 HOST = socket.gethostname()
 
-# Serve folder static/
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-def root():
-    # Kirim UI
+def ui():
     return FileResponse("static/index.html")
 
 @app.get("/api/info")
 def info():
-    return {
+    return JSONResponse({
         "ok": True,
         "hostname": HOST,
         "service": os.getenv("RAILWAY_SERVICE_NAME", "unknown"),
-    }
+    })
 
 @app.get("/work")
 def work(ms: int = Query(300, ge=0, le=5000)):
