af8a9fd1cff0fcd92b3b53e3bb7048eb9c2bcb90
mamirs22 <2311102233@ittelkom-pwt.ac.id>
2025-12-31 14:22:34 +0700

    update lagi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Query
-from fastapi.responses import FileResponse, JSONResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import time, socket, os
 
@@ -13,12 +13,12 @@ def ui():
     return FileResponse("static/index.html")
 
 @app.get("/api/info")
-def info():
-    return JSONResponse({
+def api_info():
+    return {
         "ok": True,
         "hostname": HOST,
         "service": os.getenv("RAILWAY_SERVICE_NAME", "unknown"),
-    })
+    }
 
 @app.get("/work")
 def work(ms: int = Query(300, ge=0, le=5000)):
